c9dea2f3f23148f8cf04b9fcaa112f4433ae5352
Addison Hu <huisaddison@users.noreply.github.com>
2018-11-06 19:32:37 -0500

    update headshot

--- generate_html.py
+++ generate_html.py
@@ -83,7 +83,7 @@ HeaderText = HEADER_TEXT_TEMPLATE.format(
     cv              = 'pdfs/AddisonHu_CV.pdf',
     headshot_url    = 'blog/cycling-seattle.html',
     headshot_text   = 'with my bicycle',
-    headshot        = 'img/schwinn-world-sport.jpg',
+    headshot        = 'img/autumn-bicycle.jpg',
     mail_href       = r'mail&#116;o&#58;&#109;a&#37;&#54;9&#108;%40hu&#105;' +
                       r'saddiso%&#54;E&#46;c&#111;m',
     mail            = r'mail&#64;&#104;&#117;i&#115;a&#100;di&#115;&#111;&#110;&' +
